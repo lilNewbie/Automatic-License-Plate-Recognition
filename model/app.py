cc76e35e6541aa5240f3dace9a2d1d2ca09564ae
Alan Jacob Anil <90834922+lilNewbie@users.noreply.github.com>
2023-05-10 09:46:30 +0530

    Update app.py

--- model/app.py
+++ model/app.py
@@ -1,18 +1,30 @@
 from detection import detection
 import cv2
+import pafy
+def app(source, option):
+    if option=='YouTube':
+        url = source
+        vPafy = pafy.new(url)
+        play = vPafy.getbest(preftype="webm")
+        source = play
+        return 0
 
-def app(source):
-    cap = cv2.VideoCapture(source)
-    while cap.isOpened(): 
-        frameName = "frame.jpg"
-        ret, frame = cap.read()
-        if ret:
-            cv2.imwrite(frameName,frame)
-            detection("frame.jpg")
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            return 0
+    elif option=="image":
+        detection(source)
+        return 0
+    
+    else:
+        cap = cv2.VideoCapture(source)
+        while cap.isOpened(): 
+            frameName = "frame.jpg"
+            ret, frame = cap.read()
+            if ret:
+                cv2.imwrite(frameName,frame)
+                detection("frame.jpg")
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                return 0
     cap.release()
     cv2.destroyAllWindows()
 
