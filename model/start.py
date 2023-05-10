d67dea200ce0bdb90e35e5e4bb435dca1de53be3
Alan Jacob Anil <90834922+lilNewbie@users.noreply.github.com>
2023-05-10 09:43:15 +0530

    Update start.py

--- model/start.py
+++ model/start.py
@@ -9,7 +9,15 @@ class Form(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.name_label = tk.Label(self, text="Input video:")
+        self.option_label = tk.Label(self, text="Input Type:")
+        self.option_label.pack()
+        self.option_var = tk.StringVar(self)
+        self.option_var.set("Local file")
+        self.option_dropdown = tk.OptionMenu(
+            self, self.option_var, "Local file", "YouTube", "Image")
+        self.option_dropdown.pack()
+
+        self.name_label = tk.Label(self, text="Input:")
         self.name_label.pack()
         self.name_entry = tk.Entry(self)
         self.name_entry.pack()
@@ -24,7 +32,8 @@ class Form(tk.Frame):
 
     def submit_form(self):
         source = self.name_entry.get()
-        app(source)
+        option = self.option_var.get()
+        app(source, option)
         self.master.destroy()
         
 root = tk.Tk()
